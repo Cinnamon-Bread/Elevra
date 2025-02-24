d8a742e3d1a49a1d87f59e731f0ce3c58c7a22eb
cinnamon-bread <ndlebezikhanyilanga@gmail.com>
2025-02-24 11:44:18 +0000

    File structure update

--- src/components/Habit/Habits.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Habits = () => {
-  return (
-    <div>Habits</div>
-  )
-}
-
--- src/components/Leaderboards/Leaderboards.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Leaderboards = () => {
-  return (
-    <div>Leaderboards</div>
-  )
-}
